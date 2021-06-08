0e299eadd59b60f2d9e4513e73b6152d5214989c
LAPTOP-R8TAHJ1O\mac95 <mac95860@hotmail.com>
2021-06-08 16:15:18 -0500

    owofied

---
+++ owofied-a-sentence.js
@@ -0,0 +1,11 @@
+// Create a function that takes a sentence and turns every "i" into "wi" and "e" into "we", and add "owo" at the end.
+
+const string1 = "I'm gonna ride 'til I can't no more";
+const string2 = "Do you ever feel like a plastic bag";
+const string3 = "Cause baby you're a firework";
+
+const owofied = str => console.log( str.replace("i", "wi").replace("e", "we") + " owo" );
+
+owofied(string1);
+owofied(string2);
+owofied(string3);
