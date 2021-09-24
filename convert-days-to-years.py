fac74cd90765395e6b74e2c8d7cebc941d8816ab
LAPTOP-R8TAHJ1O\mac95 <mac95860@hotmail.com>
2021-09-24 14:59:27 -0500

    convert age to days

---
+++ convert-days-to-years.py
@@ -0,0 +1,8 @@
+# Create a function that takes the age in years and returns the age in days.
+
+def calc_age(n):
+    return n * 365
+
+print(calc_age(65)) # 23725
+print(calc_age(0)) # 0
+print(calc_age(20)) # 7300
