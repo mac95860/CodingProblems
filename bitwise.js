464e22c1f87d8758ebf25a573a85c6ea5e15dced
LAPTOP-R8TAHJ1O\mac95 <mac95860@hotmail.com>
2021-11-17 19:50:21 -0600

    bitwise

---
+++ bitwise.js
@@ -0,0 +1,35 @@
+// A decimal number can be represented as a sequence of bits. To illustrate:
+
+// 6 = 00000110
+// 23 = 00010111
+// From the bitwise representation of numbers, we can calculate the bitwise AND, bitwise OR and bitwise XOR. Using the example above:
+
+// bitwiseAND(6, 23) ➞ 00000110
+
+// bitwiseOR(6, 23) ➞ 00010111
+
+// bitwiseXOR(6, 23) ➞ 00010001
+// Write three functions to calculate the bitwise AND, bitwise OR and bitwise XOR of two numbers.
+
+// Examples
+// bitwiseAND(7, 12) ➞ 4
+
+// bitwiseOR(7, 12) ➞ 15
+
+// bitwiseXOR(7, 12) ➞ 11
+
+const bitwiseAND = (n1, n2) => n1 & n2;
+const bitwiseOR = (n1, n2) => n1 | n2;
+const bitwiseXOR = (n1, n2) => n1 ^ n2;
+
+console.log(
+    bitwiseAND(7, 12) // 4
+)
+
+console.log(
+    bitwiseOR(7, 12) //15
+)
+
+console.log(
+    bitwiseXOR(7,12) // 11
+)
